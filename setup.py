6ca62617c550ae77500e5d11797c6527e40ccfd3
Amin Alam <ma.alamalhoda@gmail.com>
2023-12-20 12:00:18 +0100

    new version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read_description():
 
 setup(
     name="ElecPhys",
-    version="0.0.3",
+    version="0.0.4",
     author='Amin Alam',
     description='Electrophysiology data processing',
     long_description=read_description(),
