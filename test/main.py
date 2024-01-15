8b84be81b6e8dcbd014df019cf8e16007ace0b43
Amin Alam <ma.alamalhoda@gmail.com>
2024-01-15 13:22:18 +0100

    path append before the imports

--- test/main.py
+++ test/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import shutil
 import unittest
 import elecphys.conversion as conversion
@@ -5,10 +10,7 @@ import elecphys.preprocessing as preprocessing
 import elecphys.fourier_analysis as fourier_analysis
 import elecphys.visualization as visualization
 import elecphys.data_io as data_io
-import sys
-import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 
 class TestCases_0_conversion(unittest.TestCase):
